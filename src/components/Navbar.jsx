ab173ba70d7089d81e6d6921f722f9ff29e92667
Devin Sharpe <devinrsharpe@gmail.com>
2021-06-25 16:59:35 -0400

    feat: added a minimize button to the navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,13 @@ import {
   BellIcon,
   DashboardIcon,
   MenuIcon,
+  MinimizeIcon,
   SettingsIcon,
   TagIcon,
 } from "@iconicicons/react";
+import { useRecoilValue } from "recoil";
+
+import { settingsState } from "../recoil/atoms";
 
 export const NavItem = (props) => {
   let location = useLocation();
@@ -26,28 +30,45 @@ export const NavItem = (props) => {
 };
 
 const Navbar = () => {
+  const settings = useRecoilValue(settingsState);
+
   return (
     <nav className="h-screen bg-gray-800 p-4 space-y-8">
-      {/* <NavItem
-        to="/"
-        icon={<DashboardIcon className="h-12 w-12 text-white" />}
-      /> */}
+      {settings.pages.dashboard && (
+        <NavItem
+          to="/"
+          icon={<DashboardIcon className="h-12 w-12 text-white" />}
+        />
+      )}
+      {settings.pages.recent && (
+        <NavItem
+          to="/sales"
+          icon={<MenuIcon className="h-12 w-12 text-white" />}
+        />
+      )}
+      {settings.pages.inventory && (
+        <NavItem
+          to="/inventory"
+          icon={<TagIcon className="h-12 w-12 text-white" />}
+        />
+      )}
+      {settings.pages.notifications && (
+        <NavItem
+          to="/notifications"
+          icon={<BellIcon className="h-12 w-12 text-white" />}
+        />
+      )}
+
       <NavItem
-        to="/sales"
-        icon={<MenuIcon className="h-12 w-12 text-white" />}
-      />
-      <NavItem
-        to="/inventory"
-        icon={<TagIcon className="h-12 w-12 text-white" />}
-      />
-      <NavItem
-        to="/notifications"
-        icon={<BellIcon className="h-12 w-12 text-white" />}
-      />
-      {/* <NavItem
         to="/settings"
         icon={<SettingsIcon className="h-12 w-12 text-white" />}
-      /> */}
+      />
+      <button
+        onClick={() => window.ipc.send("minimize")}
+        className="p-2 block rounded-xl bg-gray-900"
+      >
+        <MinimizeIcon className="h-12 w-12 text-white" />
+      </button>
     </nav>
   );
 };
